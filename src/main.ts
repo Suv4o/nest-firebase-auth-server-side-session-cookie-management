63b741672667cf997e0664644f0ac6d29f7f18ed
Aleks <aleksandar.trpkovski@gmail.com>
2022-09-14 20:08:20 +1000

    Initialisation with the core packages installed

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      validatorPackage: require('@nestjs/class-validator'),
+      transformerPackage: require('@nestjs/class-transformer'),
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
